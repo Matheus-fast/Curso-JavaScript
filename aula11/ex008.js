8fb6e911732b77abe6268b1cc07315560cdf7799
Matheus-Fast <matheusoliveirale2007@gmail.com>
2021-05-25 21:00:27 -0300

    Arquivos de aulas ate a Aula 13

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 78.2
+
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if(vel > 60) { // Condiçao simples
+    console.log(`Voçê foi multado! O limite de velocidade é de 60 Km/h`)
+}else 
+console.log(`Dirija sempre usando cinto de segurança!`)
